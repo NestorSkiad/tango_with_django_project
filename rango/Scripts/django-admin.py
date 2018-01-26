f8c8b79fa5912a53c69e2327da411fbe5a4ac181
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-01-26 18:03:36 +0000

    TWD ch6 3.0

--- rango/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\tekno_000\Desktop\tango_with_django_project\rango\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
