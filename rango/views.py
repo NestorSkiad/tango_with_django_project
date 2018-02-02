53492605439bd6f83e6f174951cc8f11e244e135
Nestor Skiadas <2258257s@student.gla.ac.uk>
2018-02-02 16:59:05 +0000

    TWD 7.0 todo exercises

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 
 def index(request):
-
-
-
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
@@ -15,10 +13,10 @@ def index(request):
 
 
 def about(request):
-
     context_dict = {'name': 'Nestor Skiadas'}
 
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -33,3 +31,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
